6e78c752f37c11f35a26262647d48156499d3b52
DhruvilDudhiya <dhruvil.dudhiya@yudiz.com>
2024-07-02 18:45:36 +0530

    level 1 updated

--- src/prefabs/Board.js
+++ src/prefabs/Board.js
@@ -6,7 +6,7 @@ class Board extends Phaser.GameObjects.Container {
 
 	constructor(scene, x, y) {
 		super(scene, x ?? 0, y ?? 0);
-        
+
 		// container_boardBackGround
 		const container_boardBackGround = scene.add.container(0, 0);
 		this.add(container_boardBackGround);
@@ -17,21 +17,24 @@ class Board extends Phaser.GameObjects.Container {
 		container_boardBackGround.add(boardBackGround);
 
 		// peg
-		const peg = scene.add.image(-93, -172, "peg");
+		const peg = scene.add.image(-80, -140, "peg");
 		peg.scaleX = 0.4;
 		peg.scaleY = 0.4;
+		peg.visible = false;
 		container_boardBackGround.add(peg);
 
 		// peg_1
-		const peg_1 = scene.add.image(94.22081756591797, -172.797119140625, "peg");
+		const peg_1 = scene.add.image(0, -140, "peg");
 		peg_1.scaleX = 0.4;
 		peg_1.scaleY = 0.4;
+		peg_1.visible = false;
 		container_boardBackGround.add(peg_1);
 
 		// peg_2
-		const peg_2 = scene.add.image(12, -173, "peg");
+		const peg_2 = scene.add.image(80, -140, "peg");
 		peg_2.scaleX = 0.4;
 		peg_2.scaleY = 0.4;
+		peg_2.visible = false;
 		container_boardBackGround.add(peg_2);
 
 		// container_blankPeg
@@ -74,6 +77,7 @@ class Board extends Phaser.GameObjects.Container {
         this.container_blankPeg.removeAll(true);
     }
 	createPegs() {
+		console.log(this.blankPegPos);
         for (let pos of this.blankPegPos) {
             const { row, col, x, y } = pos;
             if (this.board[row][col] !== null) {
@@ -91,70 +95,175 @@ class Board extends Phaser.GameObjects.Container {
     }
 	onPegClick(pointer, gameObject) {
         if (!gameObject.boardPosition) return;
+		this.clearSuggestions();
 
         if (this.selectedPeg === null) {
             if (this.board[gameObject.boardPosition.row][gameObject.boardPosition.col] === 1) {
                 this.selectedPeg = gameObject;
                 this.selectedPeg.setTint(0x00ff00); // Highlight selected peg
+
+				const possibleJumps = this.findPossibleJumps(gameObject.boardPosition.row, gameObject.boardPosition.col);
+            this.showSuggestions(possibleJumps);
             }
         } else {
             if (this.canJump(this.selectedPeg.boardPosition, gameObject.boardPosition)) {
                 this.jump(this.selectedPeg.boardPosition, gameObject.boardPosition);
                 this.selectedPeg.clearTint();
                 this.selectedPeg = null;
-                // this.checkGameOver();
+                this.checkGameOver();
+				this.clearSuggestions();
+				
             } else {
                 // If invalid move, deselect the peg
                 this.selectedPeg.clearTint();
                 this.selectedPeg = null;
+				this.clearSuggestions();
             }
         }
     }
+	showSuggestions(possibleJumps) {
+		for (let jump of possibleJumps) {
+			const peg = this.getPegAtPosition(jump);
+			if (peg) {
+				peg.setTint(0x0000ff); // Highlight possible jumps in blue
+			}
+		}
+	}
+	
+	clearSuggestions() {
+		this.container_blankPeg.getAll().forEach(peg => peg.clearTint());
+	}
 	canJump(from, to) {
-        const dx = to.col - from.col;
-        const dy = to.row - from.row;
-        
-        if ((Math.abs(dx) === 2 && dy === 0) || (Math.abs(dy) === 2 && dx === 0)) {
-            const middleRow = from.row + dy / 2;
-            const middleCol = from.col + dx / 2;
-            return (
-                this.board[from.row][from.col] === 1 &&
-                this.board[middleRow][middleCol] === 1 &&
-                this.board[to.row][to.col] === 0
-            );
-        }
-        return false;
-    }
+		const dx = to.col - from.col;
+		const dy = to.row - from.row;
+		
+		// Check for horizontal, vertical, and diagonal jumps
+		if ((Math.abs(dx) === 2 && Math.abs(dy) === 0) || // Horizontal
+			(Math.abs(dy) === 2 && Math.abs(dx) === 0) || // Vertical
+			(Math.abs(dx) === 2 && Math.abs(dy) === 2))   // Diagonal
+		{
+			const middleRow = from.row + dy / 2;
+			const middleCol = from.col + dx / 2;
+			
+			// Check if all positions are within the board boundaries
+			if (this.isValidPosition(from.row, from.col) &&
+				this.isValidPosition(middleRow, middleCol) &&
+				this.isValidPosition(to.row, to.col)) {
+				
+				return (
+					this.board[from.row][from.col] === 1 &&
+					this.board[middleRow][middleCol] === 1 &&
+					this.board[to.row][to.col] === 0
+				);
+			}
+		}
+		return false;
+	}
+	isValidPosition(row, col) {
+		return row >= 0 && row < this.board.length &&
+			   col >= 0 && col < this.board[row].length &&
+			   this.board[row][col] !== null;
+	}
 
-    jump(from, to) {
-        const middleRow = (from.row + to.row) / 2;
-        const middleCol = (from.col + to.col) / 2;
-
-        // Update board state
-        this.board[from.row][from.col] = 0;
-        this.board[middleRow][middleCol] = 0;
-        this.board[to.row][to.col] = 1;
-
-        // Update visuals
-        this.updatePegVisuals(from, 0);
-        this.updatePegVisuals({row: middleRow, col: middleCol}, 0);
-        this.updatePegVisuals(to, 1);
-    }
-
-    updatePegVisuals(position, state) {
-        const pegToUpdate = this.getPegAtPosition(position);
-        if (pegToUpdate) {
-            if (state === 1) {
-                pegToUpdate.setTexture('peg');
-                this.container_blankPeg.remove(pegToUpdate);
-                this.pegContainer.add(pegToUpdate);
-            } else {
-                pegToUpdate.setTexture('peg-blank');
-                this.pegContainer.remove(pegToUpdate);
-                this.container_blankPeg.add(pegToUpdate);
-            }
-        }
-    }
+	jump(from, to) {
+		const middleRow = (from.row + to.row) / 2;
+		const middleCol = (from.col + to.col) / 2;
+	
+		// Update board state
+		this.board[from.row][from.col] = 0;
+		this.board[middleRow][middleCol] = 0;
+		this.board[to.row][to.col] = 1;
+	
+		// Update peg visuals
+		this.updatePegVisuals(from, 0);
+		this.updatePegVisuals({row: middleRow, col: middleCol}, 0);
+		this.updatePegVisuals(to, 1);
+	
+		// Check for game over after the jump
+		this.checkGameOver();
+	}
+	// jump(from, to) {
+	// 	const middleRow = (from.row + to.row) / 2;
+	// 	const middleCol = (from.col + to.col) / 2;
+	
+	// 	// Get the peg object that's jumping
+	// 	const jumpingPeg = this.getPegAtPosition(from);
+	// 	console.log(jumpingPeg);
+	
+	// 	if (jumpingPeg) {
+	// 		// Get the target position
+	// 		const targetPeg = this.getPegAtPosition(to);
+	// 		const targetX = targetPeg.x;
+	// 		const targetY = targetPeg.y;
+	
+	// 		// Create the tween for the jumping peg
+	// 		this.oScene.tweens.add({
+	// 			targets: jumpingPeg,
+	// 			x: targetX,
+	// 			y: targetY,
+	// 			duration: 300,
+	// 			ease: 'Power2',
+	// 			onComplete: () => {
+	// 				// Update board state
+	// 				this.board[from.row][from.col] = 0;
+	// 				this.board[middleRow][middleCol] = 0;
+	// 				this.board[to.row][to.col] = 1;
+	
+	// 				// Remove the jumped-over peg
+	// 				const middlePeg = this.getPegAtPosition({row: middleRow, col: middleCol});
+	// 				if (middlePeg) {
+	// 					this.pegContainer.remove(middlePeg);
+	// 					middlePeg.destroy();
+	// 				}
+	
+	// 				// Update the jumping peg's board position
+	// 				jumpingPeg.boardPosition = { row: to.row, col: to.col };
+	
+	// 				// Check for game over after the jump is complete
+	// 				this.checkGameOver();
+	// 			}
+	// 		});
+	// 	}
+	// }
+	findPossibleJumps(row, col) {
+		const directions = [
+			{dx: 2, dy: 0},  // Right
+			{dx: -2, dy: 0}, // Left
+			{dx: 0, dy: 2},  // Down
+			{dx: 0, dy: -2}, // Up
+			{dx: 2, dy: 2},  // Down-Right
+			{dx: -2, dy: -2},// Up-Left
+			{dx: 2, dy: -2}, // Up-Right
+			{dx: -2, dy: 2}  // Down-Left
+		];
+	
+		let possibleJumps = [];
+	
+		for (let dir of directions) {
+			const toRow = row + dir.dy;
+			const toCol = col + dir.dx;
+			if (this.canJump({row, col}, {row: toRow, col: toCol})) {
+				possibleJumps.push({row: toRow, col: toCol});
+			}
+		}
+	
+		return possibleJumps;
+	}
+	
+	updatePegVisuals(position, state) {
+		const pegToUpdate = this.getPegAtPosition(position);
+		if (pegToUpdate) {
+			if (state === 1) {
+				pegToUpdate.setTexture('peg');
+				this.container_blankPeg.remove(pegToUpdate);
+				this.pegContainer.add(pegToUpdate);
+			} else {
+				pegToUpdate.setTexture('peg-blank');
+				this.pegContainer.remove(pegToUpdate);
+				this.container_blankPeg.add(pegToUpdate);
+			}
+		}
+	}
 
 	getPegAtPosition(position) {
 		let peg = null;
@@ -166,7 +275,7 @@ class Board extends Phaser.GameObjects.Container {
 				return false; // Stop iteration
 			}
 		});
-		
+
 		if (!peg) {
 			this.container_blankPeg.iterate((child) => {
 				if (child.boardPosition && 
@@ -177,7 +286,7 @@ class Board extends Phaser.GameObjects.Container {
 				}
 			});
 		}
-		
+
 		return peg;
 	}
 	checkGameOver() {
@@ -191,22 +300,33 @@ class Board extends Phaser.GameObjects.Container {
         }
     }
 	hasValidMoves() {
-        for (let row = 0; row < this.board.length; row++) {
-            for (let col = 0; col < this.board[row].length; col++) {
-                if (this.board[row][col] === 1) {
-                    if (
-                        this.canJump({row, col}, {row: row+2, col}) ||
-                        this.canJump({row, col}, {row: row-2, col}) ||
-                        this.canJump({row, col}, {row, col: col+2}) ||
-                        this.canJump({row, col}, {row, col: col-2})
-                    ) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
+		const directions = [
+			{dx: 2, dy: 0},  // Right
+			{dx: -2, dy: 0}, // Left
+			{dx: 0, dy: 2},  // Down
+			{dx: 0, dy: -2}, // Up
+			{dx: 2, dy: 2},  // Down-Right
+			{dx: -2, dy: -2},// Up-Left
+			{dx: 2, dy: -2}, // Up-Right
+			{dx: -2, dy: 2}  // Down-Left
+		];
+	
+		for (let row = 0; row < this.board.length; row++) {
+			for (let col = 0; col < this.board[row].length; col++) {
+				if (this.board[row][col] === 1) {
+					for (let dir of directions) {
+						const toRow = row + dir.dy;
+						const toCol = col + dir.dx;
+						if (this.isValidPosition(toRow, toCol) && 
+							this.canJump({row, col}, {row: toRow, col: toCol})) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
 
 	/* END-USER-CODE */
 }
--- src/scripts/LevelManager.js
+++ src/scripts/LevelManager.js
@@ -29,26 +29,64 @@ class LevelManager {
                     { x: 0, y: 140, row: 4, col: 2 }
                 ]
             },
-            // {
-            //     name : "square",
-            //     board: [
-            //         [null, null, 1, 1, 1, null, null],
-            //         [null, null, 0, 1, 0, null, null],
-            //         [0, 0, 0, 1, 0, 0, 0],
-            //         [0, 1, 1, 1, 1, 1, 0],
-            //         [0, 0, 0, 1, 0, 0, 0],
-            //         [null, null, 0, 1, 0, null, null],
-            //         [null, null, 0, 0, 0, null, null]
-            //     ],
-            //     pegPositions:[
-            //         { x: -55, y: -170, row: 0, col: 2 },
-            //         { x: 5, y: -170, row: 0, col: 3 },
-            //         { x: 65, y: -170, row: 0, col: 4 },
-                   
-            //     ]
-            // }
+            {
+                name: "square",
+                board: [
+                    [null, null, 1,1,1, null, null],
+                    [null, null, 1,1,1, null, null],
+                    [1, 1, 1, 1,1,1,1],
+                    [1, 1, 1, 0,1,1,1],
+                    [1, 1, 1, 1,1,1,1],
+                    [null, null, 1,1,1, null, null],
+                    [null, null, 1,1,1, null, null],
+                ],
+                pegPositions: [
+                    { x: -80, y: -230, row: 0, col: 2 },
+                    { x: 0, y: -230, row: 0, col: 3 },
+                    { x: 80, y: -230, row: 0, col: 4 },
+                    { x: -80, y: -160, row: 1, col: 2 },
+                    { x: 0, y: -160, row: 1, col: 3 },
+                    { x: 80, y: -160, row: 1, col: 4 },
+                    { x: -230, y: -90, row: 2, col: 0 },
+                    { x: -160, y: -90, row: 2, col: 1 },
+                    { x: -80, y: -90, row: 2, col: 2 },
+                    { x: 0, y: -90, row: 2, col: 3 },
+                    { x: 80, y: -90, row: 2, col: 4 },
+                    { x: 160, y: -90, row: 2, col: 5 },
+                    { x: 230, y: -90, row: 2, col: 6 },
+                    { x: -230, y: 0, row: 3, col: 0 },
+                    { x: -160, y: 0, row: 3, col: 1 },
+                    { x: -80, y: 0, row: 3, col: 2 },
+                    { x: 0, y: 0, row: 3, col: 3 },
+                    { x: 80, y: 0, row: 3, col: 4 },
+                    { x: 160, y: 0, row: 3, col: 5 },
+                    { x: 230, y: 0, row: 3, col: 6 },
+                    { x: -230, y: 90, row: 4, col: 0 },
+                    { x: -160, y: 90, row: 4, col: 1 },
+                    { x: -80, y: 90, row: 4, col: 2 },
+                    { x: 0, y: 90, row: 4, col: 3 },
+                    { x: 80, y: 90, row: 4, col: 4 },
+                    { x: 160, y: 90, row: 4, col: 5 },
+                    { x: 230, y: 90, row: 4, col: 6 },
+                    { x: -230, y: 160, row: 5, col: 0 },
+                    { x: -160, y: 160, row: 5, col: 1 },
+                    { x: -80, y: 160, row: 5, col: 2 },
+                    { x: 0, y: 160, row: 5, col: 3 },
+                    { x: 80, y: 160, row: 5, col: 4 },
+                    { x: 160, y: 160, row: 5, col: 5 },
+                    { x: 230, y: 160, row: 5, col: 6 },
+                    { x: -230, y: 230, row: 6, col: 0 },
+                    { x: -160, y: 230, row: 6, col: 1 },
+                    { x: -80, y: 230, row: 6, col: 2 },
+                    { x: 0, y: 230, row: 6, col: 3 },
+                    { x: 80, y: 230, row: 6, col: 4 },
+                    { x: 160, y: 230, row: 6, col: 5 },
+                    { x: 230, y: 230, row: 6, col: 6 },
+                    
+                ]
+            },
         ];
-        this.currentLevelIndex = 0;
+        this.currentLevelIndex = 1;
     }
 
     getCurrentLevel(){
